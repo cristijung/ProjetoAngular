de46cf4eab4454ebf62a426abae01ae729024af5
Cristina Jung <cristijung@gmail.com>
2023-04-01 16:55:52 -0300

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Telescópio James Webb';
+  description = 'Galáxias';
+  url='https://services.meteored.com/img/article/telescopio-webb-capta-con-muy-alta-resolucion-a-los-pilares-de-la-creacion-1667393426739_768.png'
 }
