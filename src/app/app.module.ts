b013ef962f4fb86bea61e56787520e104ca55494
Cristina Jung <cristijung@gmail.com>
2023-04-01 19:39:58 -0300

    componente feito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { PhotoComponent } from './components/photo/photo.component';
+import { TextComponent } from './components/text/text.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent,
+    TextComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/photo/photo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "pa-photo",
+    templateUrl: "photo.component.html"
+})
+
+export class PhotoComponent {
+    description = "Galáxias";
+    url="https://services.meteored.com/img/article/telescopio-webb-capta-con-muy-alta-resolucion-a-los-pilares-de-la-creacion-1667393426739_768.png"
+  
+}
